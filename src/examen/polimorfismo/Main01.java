3360a52faa8557c622b45ee2f280efa45712923d
DoberWho <jorge.parada@ctbarbanza.com>
2019-11-27 13:17:55 +0100

    .

--- src/examen/polimorfismo/Main01.java
+++ src/examen/polimorfismo/Main01.java
@@ -1,6 +1,6 @@
 package examen.polimorfismo;
 
-public class Main {
+public class Main01 {
 	/*
 	 * Esta clase es para probar el Polimorfismo 
 	 */
@@ -24,15 +24,14 @@ public class Main {
 		//System.out.println("HijoMayor - Padre:"+(hijoMayor instanceof Padre)); // No se Puede
 		
 		/**
-		Padre - Persona:true
-		Padre - Adulto:true
-		
-		Hijo - Persona:true
-		Hijo - Adulto:false
-		
-		HijoMayor - Persona:true
-		HijoMayor - Adulto:true
-
+			Padre - Persona:true
+			Padre - Adulto:true
+			
+			Hijo - Persona:true
+			Hijo - Adulto:false
+			
+			HijoMayor - Persona:true
+			HijoMayor - Adulto:true
 		 */
 		
 		
@@ -43,6 +42,21 @@ public class Main {
 		/**
 		 * 1.- ¿Podemos hacer instancia de Interface o Abstract?
 		 * .- No; no se puede hacer una instancia de ninguna
+		 */
+		
+		/**
+		 * 2.- ¿Un Padre es Persona y Adulto?
+		 * .- Si; Por que implementa Adulto y Extiende Persona <= TRUE
+		 */
+		
+		/**
+		 * 3.- ¿Puede un Hijo usarse donde se usa el Padre?
+		 * .- SI; Si el objeto es Persona <= TRUE
+		 */
+		
+		/**
+		 * 4.- ¿Puede un HijoMayor usarse en Padre?
+		 * .- Si; si usamos la interface Adulto o la clase abstract Persona <= TRUE
 		 */
 		
 	}
